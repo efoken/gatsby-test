4c35a7e480e13e4fa7ce3980f9b7fc4facfa81e4
Eike Foken <eike.foken@ibmix.de>
2023-06-06 13:50:51 +0200

    Install Prettier and format everything

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,20 +1,19 @@
-import type { GatsbyConfig } from "gatsby"
+import type { GatsbyConfig } from 'gatsby'
 import getProducts from './server/get-products'
-
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `eike-task`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: 'eike-task',
+    siteUrl: 'https://www.yourdomain.tld'
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [],
-  developMiddleware: app => {
+  developMiddleware: (app) => {
     app.get('/api/products/:locale', getProducts)
-  },
+  }
 }
 
 export default config
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,5 @@
-import * as React from "react"
+import * as React from 'react'
 
-export default function NotFound () {
+export default function NotFound() {
   return <h1>not found</h1>
 }
